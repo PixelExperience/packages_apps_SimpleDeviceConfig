0ccd1b58aa2be313dd1f6ebb67cc102178890acb
jhenrique09 <jhsv09@gmail.com>
2023-08-06 16:11:13 -0300

    Allow resetting namespace props on boot
    Change-Id: Ied21c47ec3f26f2d19e7053100b748cf9efb4bf2

--- src/org/protonaosp/deviceconfig/BootReceiver.java
+++ src/org/protonaosp/deviceconfig/BootReceiver.java
@@ -20,8 +20,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.DeviceConfig;
+import android.provider.Settings;
 import android.util.Log;
 import android.os.SystemProperties;
+import java.util.Arrays;
 
 public class BootReceiver extends BroadcastReceiver {
     private static final String TAG = "SimpleDeviceConfig";
@@ -35,6 +37,7 @@ public class BootReceiver extends BroadcastReceiver {
     }
 
     private void updateDefaultConfigs(Context context) {
+        resetConfigs(context, R.array.reset_namespaces);
         updateConfig(context, R.array.configs_base, false);
         updateConfig(context, R.array.configs_base_soft, true);
 
@@ -67,6 +70,14 @@ public class BootReceiver extends BroadcastReceiver {
             if (!isSoft || DeviceConfig.getString(namespace, key, null) == null) {
                 DeviceConfig.setProperty(namespace, key, value, false);
             }
+        }
+    }
+
+    private void resetConfigs(Context context, int configArray) {
+        // Reset namespace configs
+        String[] namespaces = context.getResources().getStringArray(configArray);
+        for (String namespace : namespaces) {
+            DeviceConfig.resetToDefaults(Settings.RESET_MODE_TRUSTED_DEFAULTS, namespace);
         }
     }
 }
